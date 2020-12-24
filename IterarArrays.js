f1f9a02d18cfaea5f4a3305739c8cfee0c9ea3ce
SidneyMayo <sidneymayo11@gmail.com>
2020-12-24 15:49:05 -0300

    primeiro commit

---
+++ IterarArrays.js
@@ -0,0 +1,12 @@
+var frutas = ["melancia", "acerola", "laranja", "banana"];
+console.log(frutas)
+
+frutas.map((fruta, index) => `${index} - ${fruta} `)
+
+// flat
+
+/*  Retorna todos os elementos de um array com todos os elementos de um sub-array
+ concatenados de forma recursiva de acordo com a profundidade especificada (depth)
+
+
+*/
